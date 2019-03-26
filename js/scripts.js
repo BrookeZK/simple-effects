0c419c08996b6587d59aaec4c6909d43d604a9fc
Brooke Kullberg and Reese Lee <brooke@>
2019-03-26 11:01:33 -0700

    add another toggle sections and clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
     $("#initially-hidden").fadeToggle("fast");
     $("#initially-showing").fadeToggle("fast");
   });
+
+  $(".clickableReturn").click(function() {
+    $("#initially-hidden").fadeToggle("fast");
+    $("#initially-showing").fadeToggle("fast");
+  });
+
+  $(".clickable2").click(function() {
+    $("#hidden-clown").fadeToggle("fast");
+    $("#showing-clown").fadeToggle("fast");
+  });
+
+  $(".clickableReturn2").click(function() {
+    $("#hidden-clown").fadeToggle("fast");
+    $("#showing-clown").fadeToggle("fast");
+  });
 });
